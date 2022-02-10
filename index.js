d9ffd615e556f45beb88cecb8b5911fc2eef6338
NotANewt <meegafauna@gmail.com>
2022-02-10 11:11:04 -0600

    updated questions

--- index.js
+++ index.js
@@ -5,64 +5,64 @@ const fs = require("fs");
 // TODO: Create an array of questions for user input
 const questions = [
   {
-    type: "input",
     name: "title",
+    type: "input",
     message: "Enter your project's title.",
   },
 
   {
-    type: "input",
     name: "description",
+    type: "input",
     message: "Enter a description of your application.",
   },
 
   {
-    type: "input",
     name: "installation",
+    type: "input",
     message: "Enter installation instructions.",
   },
 
   {
-    type: "input",
     name: "usage_information",
+    type: "input",
     message: "Enter usage information by describing instructions and examples for use.",
   },
 
   {
-    type: "input",
     name: "screenshot",
+    type: "input",
     message: "Enter the relative file path of a screenshot for your application.",
   },
 
   {
-    type: "input",
     name: "contribution_guidelines",
+    type: "input",
     message: "Enter any guidelines for other developers who would like to contribute to your application.",
   },
 
   {
-    type: "input",
     name: "test_instructions",
+    type: "input",
     message: "Enter examples of how others can test your application.",
   },
 
   {
-    type: "list",
     name: "license",
+    type: "list",
     message: "Choose a license for your application.",
     choices: ["Apache License 2.0", "GNU General Public License v3.0", "MIT License", "BSD 2-Clause 'Simplified' License", "BSD 3-Clause 'New' or 'Revised' License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser General Public License v2.1", "Mozilla Public License 2.0", "The Unlicense"],
     default: "MIT License",
   },
 
   {
-    type: "input",
     name: "github",
+    type: "input",
     message: "Enter your GitHub username.",
   },
 
   {
-    type: "input",
     name: "email",
+    type: "input",
     message: "Enter your email address.",
   },
 ];
